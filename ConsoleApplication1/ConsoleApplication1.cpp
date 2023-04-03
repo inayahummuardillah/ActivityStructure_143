5cbbc1ae143822bdc0c69fbd4786d9cf8d0bd9eb
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-04-03 10:04:09 +0630

    input mhs1, mhs2

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -17,4 +17,23 @@ int main() {
 	mhs1.nama = "Adrian Mulyo";
 	mhs1.alamat = "Wonosobo";
 	mhs1.umur = 20;
+
+	cout << "Masukan NIM :";
+	cin >> mhs2.NIM;
+	cout << "Masukan nama :";
+	cin >> mhs2.nama;
+	cout << "Masukan alamat :";
+	cin >> mhs2.alamat;
+	cout << "Masukan umur :";
+	cin >> mhs2.umur;
+
+	cout << "\nNIM = " << mhs1.NIM;
+	cout << "\nnama = " << mhs1.nama;
+	cout << "\nalamat = " << mhs1.alamat;
+	cout << "\numur = " << mhs1.umur;
+
+	cout << "\n\nNIM = " << mhs2.NIM;
+	cout << "\nnama = " << mhs1.nama;
+	cout << "\nalamat = " << mhs1.alamat;
+	cout << "\numur = " << mhs1.umur;
 }
