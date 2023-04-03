994968bde6988c2247bd4c0a7ce9650f4959f0ea
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-04-03 10:56:22 +0630

    int main fungsi for kedua

--- Latihan3/Latihan3.cpp
+++ Latihan3/Latihan3.cpp
@@ -32,4 +32,13 @@ int main() {
 		cin >> mhs[i].umur;
 		cin.ignore(1, '\n');
 	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "\n\nNIM = " << mhs[i].NIM;
+		cout << "\nNama = " << mhs[i].nama;
+		cout << "\nDesa = " << mhs[i].alamat.desa;
+		cout << "\nKota = " << mhs[i].alamat.kota;
+		cout << "\nUmur = " << mhs[i].umur;
+	}
 }
